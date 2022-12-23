78a6da3ed9c1bc1cbf11f42055e3364bbb06acee
Wonseok Park <wpark920@gmail.com>
2022-12-23 15:47:12 -0800

    some tweaks to server database files

--- server/database/MySQL/schema.sql
+++ server/database/MySQL/schema.sql
@@ -9,31 +9,49 @@ CREATE TABLE Products (
 
 CREATE TABLE Questions (
   id INT AUTO_INCREMENT,
-  body VARCHAR(10000),
-  asker_name VARCHAR(40),
-  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  helpfulness NUMBER DEFAULT 0,
-  reported BOOLEAN DEFAULT false,
   product_id INT NOT NULL,
+  body VARCHAR(10000),
+  date_written TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  asker_name VARCHAR(40),
+  asker_email VARCHAR(40),
+  reported BOOLEAN DEFAULT false,
+  helpfulness NUMBER DEFAULT 0,
   PRIMARY KEY(id),
   FOREIGN KEY (product_id) REFERENCES Products(id)
 );
 
 CREATE TABLE Answers (
   id INT AUTO_INCREMENT,
-  body VARCHAR(10000),
-  answerer_name VARCHAR(40),
-  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  helpfulness NUMBER DEFAULT 0,
   question_id INT NOT NULL,
+  body VARCHAR(10000),
+  date_written TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  answerer_name VARCHAR(40),
+  answerer_email VARCHAR(40),
+  reported BOOLEAN DEFAULT false,
+  helpfulness NUMBER DEFAULT 0,
   PRIMARY KEY(id),
   FOREIGN KEY (question_id) REFERENCES Questions(id)
 );
 
 CREATE TABLE Photos (
   id INT AUTO_INCREMENT,
-  url VARCHAR(2083),
   answer_id INT NOT NULL,
+  url VARCHAR(2083),
   PRIMARY KEY(id),
   FOREIGN KEY (answer_id) REFERENCES Answers(id)
 );
+
+COPY Questions
+FROM ''
+DELIMITER ','
+CSV HEADER;
+
+COPY Answers
+FROM ''
+DELIMITER ','
+CSV HEADER;
+
+COPY Photos
+FROM ''
+DELIMITER ','
+CSV HEADER;
